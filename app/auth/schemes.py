8622aaa84e761af317f96618b19b7349d8834a48
AK12210 <156789576+AK12210@users.noreply.github.com>
2026-07-09 14:45:42 +0500

    Add files via upload

--- app/auth/schemes.py
+++ app/auth/schemes.py
@@ -2,17 +2,12 @@ from pydantic import BaseModel, Field
 import uuid
 from datetime import datetime
 from typing import List
-
-
-
-
 class UserCreateModel(BaseModel):
     first_name: str = Field(max_length=10)
     last_name: str = Field(max_length=10)
     username: str = Field(max_length=20)
     email: str = Field(max_length=30)
     password: str = Field(min_length=8)
-
 class UserModel(BaseModel):
     uid : uuid.UUID
     username: str
@@ -23,29 +18,15 @@ class UserModel(BaseModel):
     password_hash: str = Field(exclude=True)
     created_at: datetime
     updated_at: datetime
-
-class UserResponse(BaseModel):
-    uid : uuid.UUID
-    username: str
-    email: str
-    first_name: str
-    last_name: str
-    is_verified: bool
-    created_at: datetime
-    updated_at: datetime
-
-
 class UserLoginModel(BaseModel):
     email: str = Field(max_length=30)
     password: str = Field(min_length=8)
-    
 
 class EmailModel(BaseModel):
     addresses: List[str]
-
 class PasswordResetRequestModel(BaseModel):
     email: str
-        
+
 class PasswordResetConfirmModel(BaseModel):
     new_password: str
     confirm_new_password: str
